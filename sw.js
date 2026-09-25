7fb7bfc03665840fd91640a1769b602e99406525
mikebrown747-spec <mike.brown747@gmail.com>
2026-09-25 02:18:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ const ASSETS = [
   "./styles.css",
   "./app.js",
   "./manifest.json",
-  "./icons/icon.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
